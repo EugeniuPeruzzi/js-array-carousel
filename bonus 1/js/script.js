0beb00230de61d85f37a77d49959fdd9c27e347c
EugeniuPeruzzi <jaka1997@live.it>
2023-05-24 19:28:56 +0200

    bonus 1 done

---
+++ bonus 1/js/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let immages = [
+    '/img/01.webp',
+    '/img/02.webp',
+    '/img/03.webp',
+    '/img/04.webp',
+    '/img/05.webp',
+];
+
+let display = document.querySelector('.slider');
+let slider_img = '';
+
+for (let i=0; i<immages.length; i++){
+    let displayImg = immages[i];
+    slider_img += `
+        <div class="img-container">
+            <img class="img" src="${displayImg}" alt="spiderman">
+        </div>`;   
+    };
+
+display.innerHTML = slider_img ;
+
+
+
+let dBlock = 0;
+
+let pictures = document.getElementsByClassName('img'); 
+
+pictures[dBlock].classList.add('d-block');
+
+
+
+let next = document.querySelector('.next');
+
+next.addEventListener('click', function(){
+
+    if (dBlock < immages.length - 1){
+        pictures[dBlock].classList.remove('d-block');
+        dBlock ++
+        pictures[dBlock].classList.add('d-block');
+    }
+    else{
+        pictures[dBlock].classList.remove('d-block');
+        dBlock=0
+        pictures[dBlock].classList.add('d-block');
+    }
+});
+
+let back = document.querySelector('.back');
+back.addEventListener('click', function(){
+
+    if (dBlock > 0){
+        pictures[dBlock].classList.remove('d-block');
+        dBlock--
+        pictures[dBlock].classList.add('d-block');
+    }
+    else if (dBlock == 0) {
+        pictures[dBlock].classList.remove('d-block');
+        dBlock = immages.length - 1
+        pictures[dBlock].classList.add('d-block');
+    }
+
+});
